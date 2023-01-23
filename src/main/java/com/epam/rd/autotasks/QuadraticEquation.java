7704822e1d05238e8200c4e7c92fe8c6f798ca5f
Surya <suryasekhardaripa60461@gmail.com>
2023-01-23 23:06:11 +0530

    QuadraticEquation done

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -2,6 +2,7 @@ package com.epam.rd.autotasks;
 
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.*;
 
 import static java.lang.Math.sqrt;
 
@@ -12,7 +13,16 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
-
+        if(((b*b)-(4*a*c))<0){
+            System.out.println("no roots");
+        }
+        else if(((b*b)-(4*a*c))==0){
+            System.out.println((-b)/(2*a));
+        }
+        else{
+            System.out.print(((-b)-sqrt((b*b)-(4*a*c)))/(2*a)+" ");
+            System.out.println(((-b)+sqrt((b*b)-(4*a*c)))/(2*a));
+        }
 
     }
 
